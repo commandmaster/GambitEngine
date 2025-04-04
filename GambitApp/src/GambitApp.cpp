bb8917b1bbd3895e8faaa02eda9dcfd2bf9262a8
bennettf <friesenbennett@gmail.com>
2025-04-04 10:47:56 -0600

    working app, ai UI is not setup yet

--- GambitApp/src/GambitApp.cpp
+++ GambitApp/src/GambitApp.cpp
@@ -13,30 +13,6 @@
 #include <windows.h>
 #include <cmath>
 
-double GetScreenDPI() {
-    // Get screen resolution
-    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-    
-    // Get physical size in millimeters
-    HDC screen = GetDC(NULL);
-    int screenWidthMM = GetDeviceCaps(screen, HORZSIZE);
-    int screenHeightMM = GetDeviceCaps(screen, VERTSIZE);
-    ReleaseDC(NULL, screen);
-    
-    // Convert mm to inches (1 inch = 25.4 mm)
-    double screenWidthInches = screenWidthMM / 25.4;
-    double screenHeightInches = screenHeightMM / 25.4;
-    
-    // Calculate diagonal size in inches
-    double diagonalSizeInches = std::sqrt(screenWidthInches * screenWidthInches + screenHeightInches * screenHeightInches);
-    
-    // Calculate diagonal resolution in pixels
-    double diagonalResolution = std::sqrt(screenWidth * screenWidth + screenHeight * screenHeight);
-    
-    // Calculate DPI
-    return diagonalResolution / diagonalSizeInches;
-}
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
@@ -46,8 +22,7 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 	spec.CustomTitlebar = true;
 	spec.IconPath = "assets/appLogo.png";
 
-    spec.UIScale = 0.6f * (GetScreenDPI() / 96.f);
-
+    spec.UIScale = 1.f;
 	Walnut::Application* app = new Walnut::Application(spec);
 
 
